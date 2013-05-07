2e16b10e2b8da836604bbbc7cfaeea9c27dea522
Riveascore <agaxx010@morris.umn.edu>
2013-05-07 09:22:03 -0500

    Almost have file_handler stuff all finished, yayyyyy

--- file_handler.erl
+++ file_handler.erl
@@ -1,6 +1,6 @@
 -module(file_handler).
 %-export([file_reader/2,make_file_readers/1,normalize_list/1,index/1,processFile/1,processChunks/2,processChunk/2]).
--export([normalize_list/1, processFile/2, getChunk/4, processLine/2, splitListToList/2]).
+-export([normalize_list/1, processFile/2, getChunk/4, processLine/2]).
 
 normalize_list(List) ->
     lists:map(fun(Word) -> string:to_lower(Word) end, List).
@@ -16,11 +16,7 @@ getChunk(IoDevice, 0, ChunkList, ChunkSize) ->
 	Line ->
 	    NextLineList = processLine(Line, true),
 	    NewList = lists:append(ChunkList, NextLineList),
-
-	    %comment out following line
-	    io:fwrite("ChunkList ~s~n", [[NewList]]),
-
-	    %triplet_handler:send_chunk_off(NewList),
+	    triplet_handler:send_chunk_off(NewList),
 	    FreshChunkList = processLine(Line, false),
 	    getChunk(IoDevice, ChunkSize-1, FreshChunkList, ChunkSize)
     end;
@@ -28,45 +24,22 @@ getChunk(IoDevice, 0, ChunkList, ChunkSize) ->
 getChunk(IoDevice, LinesLeft, ChunkList, ChunkSize) ->
     case io:get_line(IoDevice, "") of
 	eof ->
-	    %comment out following line
-	    io:fwrite("ChunkList ~s~n", [[ChunkList]]);
-
-	    %triplet_handler:send_chunk_off(ChunkList);
+	    triplet_handler:send_chunk_off(ChunkList);
 	Line ->
 	    NextLineList = processLine(Line, false),
 	    NewList = lists:append(ChunkList, NextLineList),
-	    io:fwrite("ahhhh chk ~s~n", [[NextLineList]]),
 	    getChunk(IoDevice, LinesLeft-1, NewList, ChunkSize)
      end.
 
-%-define(Punctuation,"(\\ |\\,|\\.|\\;|\\:|\\t|\\n|\\(|\\))+").
--define(Punctuation,"(\\ |\\,|\\.|\\;|\\:|\\t|\\n|\\(|\\))+").
-
 processLine(Line, true) ->
-    case re:split(Line, ?Punctuation) of
-	{ok, Words} ->
-	    %Normalized = normalize_list(Words),
-	    %FirstTwo = lists:sublist(Normalized, 2),
-	    FirstTwo = lists:sublist(Words, 2),
-	    FirstTwo;
-	_ -> []
-    end;
+    InitialList = re:split(Line, "(\\ |\\,|\\.|\\;|\\:|\\t|\\n|\\(|\\))+", [{return,list}]),
+    FixedList = lists:filter(fun(Entry) -> Entry /= " " end, InitialList),
+    Normalized = normalize_list(FixedList),
+    FirstTwo = lists:sublist(Normalized, 2),
+    FirstTwo;
 
 processLine(Line, false) ->
-    case re:split(Line, ?Punctuation) of
-	{ok, Words} ->
-	    %Normalized = normalize_list(Words),
-	    %Normalized;
-	    Words;
-	_ -> []
-    end.
-
-splitListToList(SplitList,EndList) ->
-    case SplitList of
-	[] -> ok;
-	[Word|Rest] ->
-	    NewList = lists:append(EndList, Word),
-	    splitListToList(Rest, NewList)
-    end.
-		
-	    
+    InitialList = re:split(Line, "(\\ |\\,|\\.|\\;|\\:|\\t|\\n|\\(|\\))+", [{return,list}]),
+    FixedList = lists:filter(fun(Entry) -> Entry /= " " end, InitialList),
+    Normalized = normalize_list(FixedList),
+    Normalized.
